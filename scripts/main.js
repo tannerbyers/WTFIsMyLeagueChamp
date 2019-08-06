d54df28238800575a04aa7ebdc46b3cddfb92649
Tanner Byers <programtanner@gmail.com>
2019-08-06 00:13:16 -0400

    moved files from orignal rep

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    // Populate using an object literal
+
+    WTF.init({
+
+        heading: [
+            "It's a fucking",
+            "Check this shit out, a fucking"
+        ],
+        response: [
+            "Already fucking seen one",
+            "Give me a-fucking-nother one",
+            "Don't really give a fuck"
+        ],
+        template: [
+            "Jungle @ChampName ",
+            "Top @ChampName ",
+            "Bot @ChampName ",
+            "Mid @ChampName ",
+            "Support @ChampName ",
+            "River @ChampName ",
+            "Full Tank @ChampName ",
+            "Support @ChampName ",
+            "Harras with @ChampName",
+            "Big Boi @ChampName",
+            "Mad Man @ChampName",
+            "@ChampName San in the Top Lane"
+        ],
+        ChampName: [
+            "Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "AurelionSol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "ChoGath", "Corki", "Darius", "Diana", "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "KhaZix", "Kindred", "Kled", "KogMaw", "LeBlanc", "LeeSin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "MissFortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Quinn", "Rakan", "Rammus", "RekSai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "TahmKench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "VelKoz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "XinZhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"
+        ]
+    });
+});
