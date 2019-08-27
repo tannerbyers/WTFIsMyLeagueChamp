46307153e314967b27e85a30e2c92797cb6023c7
tannerbyers <programtanner@gmail.com>
2019-08-27 19:33:28 -0400

    added build, along with lane list

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,8 @@ $(function() {
 
         heading: [
             "It's a fucking",
-            "Check this shit out, a fucking"
+            "Check this shit out, a fucking",
+            "What the fuck, its "
         ],
         response: [
             "Already fucking seen one",
@@ -14,21 +15,12 @@ $(function() {
             "Don't really give a fuck"
         ],
         template: [
-            "Jungle @ChampName ",
-            "Top @ChampName ",
-            "Bot @ChampName ",
-            "Mid @ChampName ",
-            "Support @ChampName ",
-            "River @ChampName ",
-            "Full Tank @ChampName ",
-            "Support @ChampName ",
-            "Harras with @ChampName",
-            "Big Boi @ChampName",
-            "Mad Man @ChampName",
-            "@ChampName San in the Top Lane"
+            "@Build @ChampName @Lane",
         ],
         ChampName: [
             "Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "AurelionSol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "ChoGath", "Corki", "Darius", "Diana", "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "KhaZix", "Kindred", "Kled", "KogMaw", "LeBlanc", "LeeSin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "MissFortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Quinn", "Rakan", "Rammus", "RekSai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "TahmKench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "VelKoz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "XinZhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"
-        ]
+        ],
+        Lane: ["Top Lane", "Jungle", "Mid", "Bot Lane", "Support", "River"],
+        Build: ["AP", "AD", "Full Tank", "Bruiser", "Utility", "Lethality", "CC"]
     });
 });
